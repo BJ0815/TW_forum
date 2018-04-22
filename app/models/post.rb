fd427b06508a11acaadd7fd5b55c495848520a5b
BJ0815 <jtt11220@gmail.com>
2018-04-22 21:59:19 +0800

    [feature- post create]add carrierwave gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  mount_uploader :photo, PhotoUploader
+
   validates :title, presence: true
   validates :description, presence: true, length: { maximum: 150 }
   validates :article_role, presence: true
