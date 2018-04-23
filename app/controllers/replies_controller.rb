1891bda0038574e21b70d0142241bb34a13917e4
BJ0815 <jtt11220@gmail.com>
2018-04-23 17:03:04 +0800

    [feature- reply destroy]

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -9,6 +9,13 @@ class RepliesController < ApplicationController
     redirect_back(fallback_location:root_path)
   end
 
+  def destroy
+    current_user.replies.where(id: params[:id]).delete_all
+    flash[:notice] = "成功刪除留言"
+    redirect_back(fallback_location: root_path)
+  end
+
+
   private
 
   def reply_params
